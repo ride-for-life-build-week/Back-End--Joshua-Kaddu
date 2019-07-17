7c8083a46bac2c378a8fb1a4dd44defd0c680bb2
jkaddu <jkaddu@yahoo.com>
2019-07-17 13:42:37 -0700

    test

--- knexfile.js
+++ knexfile.js
@@ -25,5 +25,20 @@ module.exports = {
 		migrations: {
 			directory: './data/migrations'
 		}
+	},
+	production: {
+		client: 'sqlite3',
+		useNullAsDefault: true,
+		connection: {
+			filename: './data/auth.db3'
+		},
+		pool: {
+			afterCreate: (conn, done) => {
+				conn.run('PRAGMA foreign_keys = ON', done);
+			}
+		},
+		migrations: {
+			directory: './data/migrations'
+		}
 	}
 };
